e284eb66b4f6680c9498e9e77f234e8673fa5437
mhmdrz <smrm1381@yahoo.com>
2022-09-02 16:56:28 +0430

    Fix bug python client

--- Clients/main.py
+++ Clients/main.py
@@ -1,11 +1,11 @@
-from os import getpid, makedirs
+from os import makedirs
 from enum import Enum
 
 DEBUG = 0
 
 
 class Action(Enum):
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.value)
 
     STAY = 0
@@ -23,7 +23,7 @@ class Action(Enum):
 
 
 class MapType(Enum):
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.value)
 
     EMPTY = 0
@@ -37,26 +37,26 @@ class MapType(Enum):
 
 
 class MapTile:
-    def __init__(self):
+    def __init__(self) -> None:
         self.type: MapType
         self.data: int
         self.coordinates: tuple(int, int)
 
 
 class Map:
-    def __init__(self):
-        self.width = 0
-        self.height = 0
-        self.gold_count = 0
-        self.sight_range = 0
-        self.grid = []
+    def __init__(self) -> None:
+        self.width: int
+        self.height: int
+        self.gold_count: int
+        self.sight_range: int
+        self.grid: list
 
-    def set_grid_size(self):
-        self.grid = [MapTile] * (self.sight_range ** 2)
+    def set_grid_size(self) -> None:
+        self.grid = [MapTile() for _ in range(self.sight_range ** 2)]
 
 
 class GameState:
-    def __init__(self):
+    def __init__(self) -> None:
         self.rounds = int(input())
         self.def_upgrade_cost = int(input())
         self.atk_upgrade_cost = int(input())
@@ -64,32 +64,29 @@ class GameState:
         self.linear_attack_range = int(input())
         self.ranged_attack_radius = int(input())
         self.map = Map()
-        self.map.width, self.map.height = tuple(
-            [int(x) for x in input().split()])
+        self.map.width, self.map.height = map(int, input().split())
         self.map.gold_count = int(input())
         self.map.sight_range = int(input())
         self.map.set_grid_size()
         self.debug_log = ''
 
-    def set_info(self):
-        self.location = tuple([int(x) for x in input().split()])
+    def set_info(self) -> None:
+        self.location = tuple(map(int, input().split()))  # (row, column)
         for tile in self.map.grid:
-            tile.type, tile.data, *tile.coordinates = tuple(
-                [int(x) for x in input().split()])
-        self.agent_id = int(input())
-        self.current_round = int(input())
+            tile.type, tile.data, *tile.coordinates = map(int, input().split())
+        self.agent_id = int(input())  # player1: 0,1 --- player2: 2,3
+        self.current_round = int(input())  # 1 indexed
         self.attack_ratio = float(input())
         self.deflvl = int(input())
         self.atklvl = int(input())
         self.wallet = int(input())
         self.safe_wallet = int(input())
-        self.wallets = [int(x) for x in input().split()]
+        self.wallets = [*map(int, input().split())]
 
-    def debug(self):
+    def debug(self) -> None:
         # Customize to your needs
         self.debug_log += f'round: {str(self.current_round)}\n'
         self.debug_log += f'location: {str(self.location)}\n'
-        self.debug_log += f'agent ID: {str(self.agent_id)}\n'
         self.debug_log += f'attack ratio: {str(self.attack_ratio)}\n'
         self.debug_log += f'defence level: {str(self.deflvl)}\n'
         self.debug_log += f'attack level: {str(self.atklvl)}\n'
@@ -98,14 +95,14 @@ class GameState:
         self.debug_log += f'list of wallets: {str(self.wallets)}\n'
         self.debug_log += f'{60 * "-"}\n'
 
-    def debug_file(self):
+    def debug_file(self) -> None:
         fileName = 'Clients/logs/'
         makedirs(fileName, exist_ok=True)
-        fileName += f'p{getpid()}.log'
+        fileName += f'AGENT{self.agent_id}.log'
         with open(fileName, 'a') as f:
             f.write(self.debug_log)
 
-    def get_action(self):
+    def get_action(self) -> Action:
         # write your code here
         # return the action value
         return Action.STAY
